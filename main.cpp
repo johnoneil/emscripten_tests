3a1218fb7607f90b17cda5f1b43ccc6a72beb5e8
on-three <on_three@outlook.com>
2014-11-09 14:46:36 -0800

    Value class moved through byte alignments

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #ifdef EMSCRIPTEN
 #include <emscripten.h>
 #endif
+#include <memory>
+#include <string.h>
 
 struct VS {
   int _1;
@@ -17,6 +19,10 @@ union VU {
 struct Value {
   int pointer;
   VU value;
+
+  void print() {
+
+  }
 };
 
 typedef unsigned char   Bit8u;
@@ -28,13 +34,25 @@ int main(int argc, char* argv[]) {
   //Store some values in our union-structure
   Value v;
   v.value.number = 100.0;
+  //v.value.vs._1 = 1;
+  //v.value.vs._2 = 2;
 
-  std::cout<<"Value number is "<<v.value.number<<std::endl;
+  std::cout<<"Initial Value number is "<<v.value.number<<std::endl;
+  //std::cout<<"Initial Value number 1 is "<<v.value.vs._1<<std::endl;
+  //std::cout<<"Initial Value number 2 is "<<v.value.vs._2<<std::endl;
 
-  v.value.vs._1 = 1;
-  v.value.vs._2 = 2;
-  std::cout<<"Value number 1 is "<<v.value.vs._1<<std::endl;
-  std::cout<<"Value number 2 is "<<v.value.vs._2<<std::endl;
+  //buffer for 4 Value structures.
+  for(int buffer_offset=0;buffer_offset<7;++buffer_offset) {
+    //const int buffer_offset = 0;
+    std::unique_ptr<char []> buffer(new char[sizeof(Value)*4]);
+    char* pbuffer = &buffer[buffer_offset];
+    memcpy(pbuffer,&v,sizeof(Value));
+    std::cout<<"Offset into buffer: "<<buffer_offset<<std::endl;
+    Value* pvalue = (Value*)pbuffer;
+    std::cout<<"Value number is "<<pvalue->value.number<<std::endl;
+    ///std::cout<<"Value number 1 is "<<v.value.vs._1<<std::endl;
+    //std::cout<<"Value number 2 is "<<v.value.vs._2<<std::endl;
+  }
 
   Bit8u data[4] = {0x01,0x23,0x45,0x67};
 
